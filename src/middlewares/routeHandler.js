b60a54428da9b683c3a9f4b9f596c3b49cbf3987
Pedro Duarte <pduartesilva2005@gmail.com>
2025-08-21 19:51:29 -0300

    Dynamic Params

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -6,7 +6,7 @@ const database = new Database();
 
 export function routeHandler(request, response) {
   const route = routes.find(route => {
-    return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
 
   if (route) {
